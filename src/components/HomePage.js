9fa36d621ba094bf8b209c2674bbf32a821c8690
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-03 21:03:50 +0700

    Add Link to HomePage button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/HomePage.css';
-import Salubra from '../images/salubra.png';
 
 function HomePage() {
   return (
@@ -9,7 +9,9 @@ function HomePage() {
           <div className="home-texts-container">
             <h1 className="home-heading">I can see you've started your own collection. Very nice! I'll show you some of my own, and you can take one home with you if you like!</h1>
             <p className="home-text">- Charm Lover Salubra</p>
-            <button className="shop-button">Shop Now</button>
+            <Link to="/products">
+              <button className="shop-button" type="button" >Shop Now</button> 
+            </Link>
           </div>
           <div className="home-image-container"></div>
         </div>
