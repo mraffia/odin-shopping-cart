a004797c328a1692677921057277b02ebb72a997
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-03 20:39:18 +0700

    Add HomePage content, stylings, and layouting

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/HomePage.css';
+import Salubra from '../images/salubra.png';
 
 function HomePage() {
   return (
-    <div>
-        <div>Home</div>
+    <div className="home-container">
+        <div className="home-subcontainer">
+          <div className="home-texts-container">
+            <h1 className="home-heading">I can see you've started your own collection. Very nice! I'll show you some of my own, and you can take one home with you if you like!</h1>
+            <p className="home-text">- Charm Lover Salubra</p>
+            <button className="shop-button">Shop Now</button>
+          </div>
+          <div className="home-image-container">
+            <img className="home-image" src={Salubra} alt="Salubra" />
+          </div>
+        </div>
     </div>
   );
 }
