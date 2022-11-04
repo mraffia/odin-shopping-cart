87077b656c81746533d6ec0a34cb5f3f4c7b988e
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-05 04:37:13 +0700

    Change Nav props to only handleOpenCartDisplay cause it only needs that

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
-function Nav() {
-  return (
-    <nav className="navbar">
-        <Link to="/" className="nav-title-link">
-            <div className="nav-title">Odin Shopping Cart</div>
-        </Link>
-        <ul className="nav-links">
-            <Link to="/" className="nav-link">
-                <li>Home</li>
+function Nav({ handleOpenCartDisplay }) {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="nav-title-link">
+                <div className="nav-title">Odin Shopping Cart</div>
             </Link>
-            <Link to="/products" className="nav-link">
-                <li>Products</li>
-            </Link>
-            <li>Cart</li>
-        </ul>
-    </nav>
-  );
+            <ul className="nav-links">
+                <Link to="/" className="nav-link">
+                    <li>Home</li>
+                </Link>
+                <Link to="/products" className="nav-link">
+                    <li>Products</li>
+                </Link>
+                <li className="nav-link" onClick={() => handleOpenCartDisplay()}>Cart</li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
