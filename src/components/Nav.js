2c1b4ae928367db6ae43500c9fd7548aaa2f0f22
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-05 22:26:04 +0700

    Add notification bubble elements and logic so that it shows the number of total items in the cart accurately

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Nav.css';
 
-function Nav({ handleOpenCartDisplay }) {
+function Nav({ totalItems, handleOpenCartDisplay }) {
+    let bubbleClass = "notify-bubble";
+    if (totalItems > 0) {
+        bubbleClass += " bubble-active";
+    }
+
     return (
         <nav className="navbar">
             <Link to="/" className="nav-title-link">
@@ -15,7 +20,10 @@ function Nav({ handleOpenCartDisplay }) {
                 <Link to="/products" className="nav-link">
                     <li>Products</li>
                 </Link>
-                <li className="nav-link" onClick={() => handleOpenCartDisplay()}>Cart</li>
+                <div className="notify-container">
+                    <div className={bubbleClass}>{totalItems}</div>
+                    <li onClick={() => handleOpenCartDisplay()}>Cart</li>
+                </div>
             </ul>
         </nav>
     );
