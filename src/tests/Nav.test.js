49b9e6135dd1a5ed974b34b885ee45c4ef24f6f4
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-07 01:21:31 +0700

    Revise tests for Nav component

--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -2,13 +2,13 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Nav from '../components/Nav.js';
 
 describe("Nav component", () => {
     it("calls handleOpenCartDisplay correct number of times", () => {
         const handleOpenCartDisplayMock = jest.fn();
-        render(<Nav handleOpenCartDisplay={handleOpenCartDisplayMock} />, {wrapper: MemoryRouter});
+        render(<Nav handleOpenCartDisplay={handleOpenCartDisplayMock} />, {wrapper: BrowserRouter});
         const cart = screen.getByTestId("nav-cart");
     
         userEvent.click(cart);
