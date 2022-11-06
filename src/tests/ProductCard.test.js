780105b2bfb867c1ba376fbe139e5b4f148e6917
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-07 01:40:54 +0700

    Revise tests for ProductCard component

--- src/tests/ProductCard.test.js
+++ src/tests/ProductCard.test.js
@@ -5,7 +5,7 @@ import userEvent from "@testing-library/user-event";
 import ProductCard from '../components/ProductCard.js';
 
 describe("ProductCard component", () => {
-    it("calls handleOpenCartDisplay correct number of times", () => {
+    it("calls handleAddItem correct number of times", () => {
         const item = { name: "Gathering Swarm", price: 300 };
         const handleAddItemMock = jest.fn();
         render(<ProductCard item={item} handleAddItem={handleAddItemMock} />);
