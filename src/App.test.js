d6819a5cf28d91b864e1a60f5625fd6768f55758
Mohammad Raffi Akbar <mohraffia@gmail.com>
2022-11-07 03:13:58 +0700

    Add test for App.js for when the cart modal close button is clicked

--- src/App.test.js
+++ src/App.test.js
@@ -4,36 +4,68 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe("App component", () => {
   it("renders header/nav, HomePage content, and footer", () => {
     const { container } = render(<App />);
+
     expect(container).toMatchSnapshot();
   });
 
   it("shows cart modal after cart logo in nav is clicked", () => {
     render(<App />);
-    const cart = screen.getByTestId("nav-cart");
 
-    userEvent.click(cart);
+    const navCart = screen.getByTestId("nav-cart");
+    userEvent.click(navCart);
 
-    expect(screen.getByTestId("cart-modal")).toBeVisible();
+    expect(screen.getByTestId("cart-modal")).toHaveClass('modal-active');
   });
 
-  it("shows cart notification bubble when some item's 'Add to cart' button is clicked and item goes into cart", () => {
+  it("shows no cart notification bubble there's no item in cart", () => {
     render(<App />);
+
+    expect(screen.getByTestId("notify-bubble")).not.toHaveClass('bubble-active');
+  });
+
+  it("shows cart notification bubble when user adds an item into cart", () => {
+    render(<App />);
+
     const productsPageLink = screen.getByRole("link", { name: "Products" });
     userEvent.click(productsPageLink);
-
     const addToCart = screen.getAllByRole("button", { name: "Add to cart" });
     userEvent.click(addToCart[0]);
 
-    expect(screen.getByTestId("notify-bubble")).toBeVisible();
+    expect(screen.getByTestId("notify-bubble")).toHaveClass('bubble-active');;
+    expect(screen.getByTestId("notify-bubble").textContent).toMatch('1');
+  });
+
+  it("shows the correct cart notification bubble number when user adds one or multiple items into cart", () => {
+    render(<App />);
+
+    const productsPageLink = screen.getByRole("link", { name: "Products" });
+    userEvent.click(productsPageLink);
+    const addToCart = screen.getAllByRole("button", { name: "Add to cart" });
+    userEvent.click(addToCart[0]);
+
+    expect(screen.getByTestId("notify-bubble").textContent).toMatch('1');
+    
+    userEvent.click(addToCart[1]);
+    userEvent.click(addToCart[2]);
+
+    expect(screen.getByTestId("notify-bubble").textContent).toMatch('3');
+  });
+
+  it("closes cart modal after cart modal's close button is clicked", () => {
+    render(<App />);
+
+    const navCart = screen.getByTestId("nav-cart");
+    userEvent.click(navCart);
+
+    expect(screen.getByTestId("cart-modal")).toHaveClass('modal-active');
+
+    const closeModal = screen.getByTestId("close-modal");
+    userEvent.click(closeModal);
+
+    expect(screen.getByTestId("cart-modal")).not.toHaveClass('modal-active');
   });
 
 });
